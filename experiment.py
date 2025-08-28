0b1c40102e181a6d0ccfc7d1d5df182d2deb9189
Sascha Herrmann <sascha.herrmann@zooplus.com>
2025-08-28 08:06:27 +0200

    Added display of the most compact puzzles (means TOP10% of circumference ratio)

--- experiment.py
+++ experiment.py
@@ -1,13 +1,16 @@
 import matplotlib.pyplot as plt
+import numpy as np
 from ubongo import PIECES, generate_puzzle_with_mandatory_alt
 
 
 def main():
     ratios = []
+    puzzles = []
     for seed in range(100):
+        print(f"Probing puzzle generation for seed {seed}")
         puzzle = generate_puzzle_with_mandatory_alt(
             library=PIECES,
-            k=3,
+            k=5,
             max_w=8,
             max_h=6,
             mandatory_piece="I3",
@@ -16,6 +19,7 @@ def main():
         )
         if puzzle is not None:
             ratios.append(puzzle["circumference_ratio"])
+            puzzles.append((seed,puzzle))
         else:
             print(f"No puzzle found for seed {seed}; skipping.")
 
@@ -26,6 +30,23 @@ def main():
     plt.tight_layout()
     plt.show()
 
+    # Show puzzles and their seed if they are in top 10% of circumference ratios
+    top_ratios = np.percentile(ratios, 10)
+    for i in range(len(puzzles)):
+        seed = puzzles[i][0]
+        puzzle = puzzles[i][1]
+        if puzzle["circumference_ratio"]<=top_ratios:
+            print(f"Puzzle found for seed {seed} with ratio {puzzle['circumference_ratio']}")
+            print("Constructive subset (no mandatory piece):", puzzle["subset_S"])
+            print(
+                "Alternative solutions (w/ mandatory piece):",
+                len(puzzle["alternative_solutions"]),
+            )
+            print(
+                "1st alternative solution (w/ mandatory piece):",
+                [s for s, _, _ in puzzle["alternative_solutions"][0]],
+            )
+            print("ASCII target shape:\n" + puzzle["ascii"])
 
 if __name__ == "__main__":
     main()
